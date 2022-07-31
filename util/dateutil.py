7294f5c87378e6de079ccd7ccf258fb828d047f2
emsiakkkk <emsiak131@gmail.com>
2022-07-31 22:50:01 +0800

    delete dateutil.py

--- util/dateutil.py
+++
@@ -1,9 +0,0 @@
-import time
-
-class DateUtil():
-	@staticmethod
-	def format(self, date_digi):
-	#2000/1/1 36526 946656000
-	time_stamp = (date_digi - 36526) * 24 * 60 * 60 + 946656000
-	return time.strftime("%Y-%m-%d", time.localtime(time_stamp))
-	
